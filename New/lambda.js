e536f0023e1a049b3d61e0d52de045ecb6f413e4
rahi9208 <rahi@wmail.club>
2018-08-20 10:56:42 +0530

    init

--- New/lambda.js
+++ New/lambda.js
@@ -2,9 +2,10 @@ let AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = function (event, context, callback) {
+
     ddb.get({
-        TableName: 'NumTest',
-        Key: { 'num': '456' }
+        TableName: 'NumTest2',
+        Key: { 'num': 123 }
     }).promise().then(function (data) {
         //your logic goes here
     }).catch(function (err) {
